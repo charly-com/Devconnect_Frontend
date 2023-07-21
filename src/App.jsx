a20ca2c3db98db0ab123c80110a9e60911bfb178
Charles <uchecharles223@gmail.com>
2023-07-22 00:01:35 +0100

    worked on the navbar and the theme setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-// import React from 'react'
+import { useMemo } from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import Navbar from "./component/navBar"
 
 
 function App() {
- 
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
-    <>
-      <div>
-        
-      </div>
+    <div className="app">
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      <Navbar />
+      </ThemeProvider>
+      </BrowserRouter>
+    </div>
       
-    </>
+ 
   )
 }
 
